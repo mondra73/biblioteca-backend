0f20dcbafa8e571ebf9f6db1a5a6bceb091794f7
Javier <javier.espinosa@live.com.ar>
2022-03-22 19:24:34 -0300

    Login y Registro con JWT terminado

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv').config()
+
+const app = express();
+
+// capturar body
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// Conexión a Base de datos
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.s3yiw.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
+
+// import routes
+const authRoutes = require('./routes/auth');
+const validaToken = require('./routes/validate-token');
+const admin = require('./routes/admin');
+
+// route middlewares
+app.use('/api/user', authRoutes);
+app.use('/api/admin',validaToken, admin)
+
+// iniciar server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`servidor andando en: ${PORT}`)
+})
---
+++ routes/auth.js
@@ -0,0 +1,86 @@
+const router = require('express').Router(); //esto sirve para generar una ruta en un archivo aparte (auth.js)
+const User = require('../models/Users');
+const bcrypt = require('bcrypt')
+const Joi = require('@hapi/joi');
+const jwt = require('jsonwebtoken');
+
+const schemaRegister = Joi.object({
+    name: Joi.string().min(6).max(255).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+const schemaLogin = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+router.post('/login', async (req, res) => {
+    // validaciones
+    const { error } = schemaLogin.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message })
+    
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).json({ error: true, mensaje: 'Email no registrado'});
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).json({ error: true, mensaje: 'Contraseña incorrecta' })
+
+    // Nota: En los mensajes del mail y contraseñas incorrectos, poner credenciales invalidas. Es para no darle pistas al usuario en que se esta equivocando
+
+    // Creacion del token
+    const token = jwt.sign({
+        name: user.name,
+        id: user._id
+    }, process.env.TOKEN_SECRET)
+    
+    res.header('auth-token', token).json({
+        error: null,
+        data: {token}
+    })
+    
+    res.json({
+        error: null,
+        mensaje: 'Bienvenido',
+        token
+    })
+
+})
+
+
+router.post('/register', async (req, res) => { //con post envio una peticion http al servidor (crear)
+
+    // Validaciones de Usuarios
+    const { error } = schemaRegister.validate(req.body)
+    
+    if (error) {
+        return res.status(400).json(
+            {error: error.details[0].message}
+        )
+    }
+
+    const existeElEmail = await User.findOne({email: req.body.email}) 
+    if(existeElEmail) return res.status(400).json(
+        {error: true, mensaje: 'Email ya registrado'}
+    )
+
+    const saltos = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, saltos)
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: password
+    });
+    try {
+        const userDB = await user.save();
+        res.json({
+            error: null,
+            data: userDB
+        })
+    } catch (error) {
+        res.status(400).json({error})
+    }
+})
+
+module.exports = router;
