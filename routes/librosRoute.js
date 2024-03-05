ca0aa042a843d2de0ac395d2d796f6de33ed7f6c
Your Name <you@example.com>
2024-03-05 17:14:02 -0300

    Agregue .get para libro por titulo

--- routes/librosRoute.js
+++ routes/librosRoute.js
@@ -23,6 +23,7 @@ router.get('/libros',[validaToken], async (req, res) => {
     }
 });
 
+// get por id del libro
 router.get('/libro/:libroId', [validaToken], async (req, res) => {
     const userId = req.user.id
     const libroId = req.params.libroId;
@@ -47,6 +48,34 @@ router.get('/libro/:libroId', [validaToken], async (req, res) => {
     }
 });
 
+// get por titulo del libro
+router.get('/libro/buscar/titulo/:titulo', [validaToken], async (req, res) => {
+    const userId = req.user.id
+    const titulo = req.params.titulo.toLowerCase().replace(/_/g, ' '); // Convertir el título proporcionado en minúsculas y reemplazar guiones bajos por espacios;
+
+    try {
+        // Verificar si el usuario existe
+        const user = await usuarios.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+    // Buscar el libro por su título (ignorando mayúsculas y minúsculas) de manera parcial
+
+            const regex = new RegExp(titulo, 'i'); // 'i' indica que la búsqueda es insensible a mayúsculas y minúsculas
+            const libro = user.libros.filter(libro => regex.test(libro.titulo.toLowerCase()));
+        if (libro.length === 0) {
+            return res.status(404).json({ message: 'Libro no encontrado' });
+        }
+
+        // Si se encuentra el libro, devolverlo como respuesta
+        res.status(200).json(libro);
+
+    } catch (error){
+        console.log(error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+});
 
 router.post('/carga-libros',[validaToken], async (req, res) => {
     try {
