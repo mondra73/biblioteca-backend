eb2132d1c18e4feaa31872cc7bd0ec1f93885bf0
Your Name <you@example.com>
2024-03-09 00:03:23 -0300

    Ver cambio de contraseña, muchos errores

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,45 @@
 const router = require('express').Router();
 const { required } = require('@hapi/joi');
 const validaToken = require('./validate-token');
+const usuarios = require('../models/Users');
 
-// 
+
+router.post('/cambiar-password', async (req, res) => {
+    // Obtener datos del cuerpo de la solicitud
+    const { usuario, contrasenaActual, nuevaContrasena } = req.body;
+  
+    // Lógica para cambiar la contraseña
+    try {
+      // Buscar el usuario en la base de datos
+      const usuarioDB = await usuarios.findOne({ usuario });
+  
+      // Verificar si la oficina existe
+      if (!usuarioDB) {
+        return res.status(404).json({ error: 'Usuario no encontrada' });
+      }
+  
+      // Verificar la contraseña actual
+      const contrasenaValida = await bcrypt.compare(contrasenaActual, usuarioDB.password);
+      if (!contrasenaValida) {
+        return res.status(401).json({ error: 'Contraseña actual incorrecta' });
+      }
+  
+      // Generar el hash de la nueva contraseña
+      const saltos = await bcrypt.genSalt(10);
+      const nuevaContrasenaHash = await bcrypt.hash(nuevaContrasena, saltos);
+  
+      // Actualizar la contraseña en la base de datos
+      usuarioDB.password = nuevaContrasenaHash;
+      await usuarioDB.save();
+  
+      res.json({
+        mensaje: 'Contraseña cambiada exitosamente',
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
 
 
 module.exports = router
