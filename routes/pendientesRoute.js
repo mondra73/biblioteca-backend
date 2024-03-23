34ff0c5bdd1c843ab6c8652a47ce4237728f8f4c
Your Name <you@example.com>
2024-03-22 23:58:23 -0300

    Le saque el valid al esquema de carga pendientes

--- routes/pendientesRoute.js
+++ routes/pendientesRoute.js
@@ -10,7 +10,7 @@ const schemaCargaPendientes = Joi.object({
     titulo: Joi.string().required().messages({
         'any.required': 'El título es obligatorio.'
     }),
-    tipo: Joi.string().valid('Libros', 'Peliculas', 'Series').required().messages({
+    tipo: Joi.string().required().messages({
         'any.only': 'El tipo debe ser Libros, Peliculas o Series.', }),
     autorDirector: Joi.string().allow('').optional(),
     descripcion: Joi.string().allow('').optional(),
