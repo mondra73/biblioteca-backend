67fb7a462434bb718de366186514c171c3eeb2b3
Your Name <you@example.com>
2024-03-22 22:20:30 -0300

    Se agrego el campo tipo a los pendientes, para saber si es un libro, pelicula o serie

--- models/pendientes.js
+++ models/pendientes.js
@@ -4,6 +4,7 @@ const pendienteSchema = mongoose.Schema({
         titulo: String,
         autorDirector: String,
         descripcion: String,
+        tipo: String,
         confirma: {
                 type: Boolean,
                 default: false
