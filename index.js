90f146bce5244e01d48244f15a214faa24091f9d
Mondra <mondra73@gmail.com>
2025-09-01 19:31:05 -0300

    agregue un nuevo subdominio para cors

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(cors({
   origin: [
     'http://localhost:5173', 
     'http://localhost:3000',
-    'https://bibliotecamultimedia.onrender.com'
+    'https://bibliotecamultimedia.onrender.com',
+    'https://biblioteca-testing.onrender.com' 
   ],
   credentials: true
 }));
